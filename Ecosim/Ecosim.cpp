4bd1f45a3bf5d7fb80fae21e5b679671fd1a2d5f
OctoRocket <xteamxeno@gmail.com>
2021-11-08 17:15:09 -0600

    .

--- Ecosim/Ecosim.cpp
+++ Ecosim/Ecosim.cpp
@@ -12,12 +12,16 @@ void sleep(int time_in_ms)
     using namespace chrono;
     sleep_for(seconds(time_in_ms));
 }
-int autorun(float RPI, float WPI, float MRP)
+
+int autorun(float RPI, int WPI, int MRP)
 {
     int rabbits = 100;
     int wolfs = 10;
-    int rounds_since_ate;
+    int rounds_since_ate = 0;
     int wait;
+    float t_chance;
+    float chance;
+    bool wolfs_eat;
     cout << "How long (in ms) do you want to wait between each round?\n";
     cin >> wait;
     while (true)
@@ -36,25 +40,42 @@ int autorun(float RPI, float WPI, float MRP)
         {
             rabbits = MRP;
         }
+        t_chance = (wolfs / rabbits) / 100;
+        if (t_chance > 1)
+        {
+            wolfs_eat = true;
+        }
+        else
+        {
+            chance = (float)rand() / RAND_MAX;
+            if (chance <= t_chance)
+            {
+                wolfs_eat = true;
+            }
+
+        }
+        wolfs = wolfs - 2 ^ rounds_since_ate;
+        sleep(wait);
     }
     cin.ignore();
     cin.ignore();
     return 0;
 }
 
-int manual_run(float RPI, float WPI, float MRP)
+int manual_run(int RPI, int WPI, int MRP)
 {
     return 0;
 }
 
 int main()
 {
-    srand(time(NULL));
+    srand((unsigned)time(NULL));
     float rabbit_pop_increase;
-    float wolf_pop_increase;
-    float max_rabbit_pop;
+    int wolf_pop_increase;
+    int max_rabbit_pop;
     bool autorun_choice;
     string answer;
+    cout << "Random numbers generated between 0 and 1:" << endl;
     while (true)
     {
         cout << "How much do rabbits reproduce? (eg. 2 is they double each round)\n";
